c5508c10e0ad64fda6ba1b66ce27f74756b6bad9
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-02-28 11:15:24 -0500

    objects done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,61 @@
   object name, diet, weight, length, period
 */
 
-// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+let tRex = {
+  name: `tyrannosaurus`,
+  diet: `carnivorous`,
+  weight: `7000kg`,
+  length: `12m`,
+  period: `Late Cretaceous`,
+  roar: function(){
+    return `RAWERSRARARWERSARARARRRR!`;
+  }
 
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+}
 
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+let steg = {
+  name: `stegosaurus`,
+  diet: `herbivorous`,
+  weight: `2000kg`,
+  length: `9m`,
+  period: `Late Jurassic`
+
+}
+
+let raptor = {
+  name: `velociraptor`,
+  diet: `carnivorous`,
+  weight: `15kg`,
+  length: `1.8m`,
+  period: `Late Cretaceous`
+
+}
+
+
+
+// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous - done
+
+// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic - done 
+
+// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous - done 
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tRex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tRex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tRex.roar());
 
 
 // ==== Arrays ====
